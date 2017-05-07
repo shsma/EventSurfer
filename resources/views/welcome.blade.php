e25668027187ab7e341066d5ef25232cf471508c
sma shady <sma shady>
2017-05-06 20:11:46 -0400

    views pages code refactoring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,6 @@
 @section('body')
 <div class="banner">
     <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @if (Auth::check())
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                @endif
-            </div>
-        @endif
 
         <div class="content">
             <div class="title m-b-md">
