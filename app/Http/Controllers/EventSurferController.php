7307874777f225105319be7cb00eedb27b2a5e92
sma shady <sma shady>
2017-05-08 16:32:05 -0400

    added auth for all pages

--- app/Http/Controllers/EventSurferController.php
+++ app/Http/Controllers/EventSurferController.php
@@ -20,7 +20,7 @@ class EventSurferController extends Controller
 
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function insertToDb(){
